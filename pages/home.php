6acc3675f91c7d254d37e68b54841affe8f58f9a
Eduardo Hernandez <joredher30@gmail.com>
2025-03-31 21:25:27 +1000

    Adding the first draft in the body

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,36 @@
 <?php
+
+$path_body_images =  "../assets/images/body_page/"
+
+?>
+
+<div class="frame-parent">
+    <div class="frame-group">
+        <div class="explore-logic-peripherals-au-parent">
+            <div class="explore-logic-peripherals">Explore Logic Peripherals AU</div>
+            <div class="frame-container">
+                <div class="parent">
+                    <div class="div">50+</div>
+                    <div class="peripherals-parts">Peripherals Parts</div>
+                </div>
+                <div class="frame-child">
+                </div>
+                <div class="parent">
+                    <div class="div">100+</div>
+                    <div class="peripherals-parts">Customers</div>
+                </div>
+            </div>
+        </div>
+        <div class="what-are-you-looking-for-parent">
+            <div class="what-are-you">What are you looking for?</div>
+            <div class="frame-item">
+            </div>
+            <img class="search-icon" alt="" src="<?php echo $path_body_images ?>Search.svg">
+        </div>
+    </div>
+    <div class="frame-inner">
+    </div>
+    <img class="image-icon" alt="" src="<?php echo $path_body_images?>image.png">
+    <img class="vector-icon" alt="" src="<?php echo $path_body_images?>Vector 186.svg">
+    <img class="frame-child1" alt="" src="<?php echo $path_body_images?>Vector 187.svg">
+</div>
