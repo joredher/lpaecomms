518e4bc9b8054efcab7c43ae9650b7af72464a93
Eduardo Hernandez <joredher30@gmail.com>
2025-08-24 19:33:38 +1000

    Make brand names clickable links

--- includes/brands.php
+++ includes/brands.php
@@ -3,9 +3,9 @@ $var = "Example";
 ?>
 
 <div class="brands">
-    <div class="samsung">SAMSUNG</div>
-    <div class="lenovo">LENOVO</div>
-    <div class="acer">ACER</div>
-    <div class="asus">ASUS</div>
-    <div class="dell">DELL</div>
+    <div class="samsung"><a href="https://www.samsung.com/">SAMSUNG</a></div>
+    <div class="lenovo"><a href="https://www.lenovo.com/">LENOVO</a></div>
+    <div class="acer"><a href="https://www.acer.com/">ACER</a></div>
+    <div class="asus"><a href="https://www.asus.com/">ASUS</a></div>
+    <div class="dell"><a href="https://www.dell.com/">DELL</a></div>
 </div>
