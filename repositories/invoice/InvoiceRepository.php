1e5d5f10fa3bfaf0fa92447c66c7eb4d39711d72
Eduardo Hernandez <joredher30@gmail.com>
2025-08-24 20:42:49 +1000

    fix invoice repository base include path

--- repositories/invoice/InvoiceRepository.php
+++ repositories/invoice/InvoiceRepository.php
@@ -1,6 +1,6 @@
 <?php
 use repositories\BaseRepository;
-require_once 'repositories/BaseRepository.php';
+loadRepo('repositories/BaseRepository.php');
 
 class InvoiceRepository extends BaseRepository
 {
