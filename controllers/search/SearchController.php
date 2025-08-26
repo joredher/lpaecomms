1f7e2f805f73d43b16e9b01d982cff172c866cea
Eduardo Hernandez <joredher30@gmail.com>
2025-08-26 21:49:42 +1000

    Include account options in live search results

--- controllers/search/SearchController.php
+++ controllers/search/SearchController.php
@@ -16,6 +16,45 @@ class SearchController
         $stmt->execute(["%{$query}%"]);
         $products = $stmt->fetchAll();
 
+        $options = [
+            [
+                'keywords' => ['order', 'orders'],
+                'label'    => 'Show Orders',
+                'url'      => '/profile#orders',
+            ],
+            [
+                'keywords' => ['cancellation', 'cancel', 'cancellations'],
+                'label'    => 'My Cancellations',
+                'url'      => '/profile#cancellations',
+            ],
+            [
+                'keywords' => ['return', 'returns'],
+                'label'    => 'My Returns',
+                'url'      => '/profile#returns',
+            ],
+            [
+                'keywords' => ['profile', 'account'],
+                'label'    => 'My Profile',
+                'url'      => '/profile#profile',
+            ],
+            [
+                'keywords' => ['address', 'addresses'],
+                'label'    => 'Address Book',
+                'url'      => '/profile#address',
+            ],
+        ];
+
+        $accounts = [];
+        $needle = strtolower($query);
+        foreach ($options as $opt) {
+            foreach ($opt['keywords'] as $kw) {
+                if (str_contains($kw, $needle) || str_contains(strtolower($opt['label']), $needle)) {
+                    $accounts[] = ['label' => $opt['label'], 'url' => $opt['url']];
+                    break;
+                }
+            }
+        }
+
         ob_start();
         include 'pages/templates/search_results.php';
         $html = ob_get_clean();
