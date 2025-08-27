d50194e49042724aee7814553949dd8f329d0b4f
Eduardo Hernandez <joredher30@gmail.com>
2025-08-27 13:18:02 +1000

    feat: add admin dashboard card grid

---
+++ pages/admin/dashboard.php
@@ -0,0 +1,81 @@
+<div class="row g-4">
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card text-center h-100">
+            <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <i class="bi bi-building display-4"></i>
+                <h6 class="mt-3">Company profile</h6>
+            </div>
+            <a href="/admin.company-profile" class="stretched-link"></a>
+        </div>
+    </div>
+
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card text-center h-100">
+            <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <i class="bi bi-bar-chart display-4"></i>
+                <h6 class="mt-3">Reports</h6>
+            </div>
+            <a href="/admin.reports" class="stretched-link"></a>
+        </div>
+    </div>
+
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card text-center h-100">
+            <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <i class="bi bi-app display-4"></i>
+                <h6 class="mt-3">Apps</h6>
+            </div>
+            <a href="/admin.apps" class="stretched-link"></a>
+        </div>
+    </div>
+
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card text-center h-100">
+            <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <i class="bi bi-people display-4"></i>
+                <h6 class="mt-3">Groups</h6>
+            </div>
+            <a href="/admin.groups" class="stretched-link"></a>
+        </div>
+    </div>
+
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card text-center h-100">
+            <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <i class="bi bi-list-check display-4"></i>
+                <h6 class="mt-3">Rules</h6>
+            </div>
+            <a href="/admin.rules" class="stretched-link"></a>
+        </div>
+    </div>
+
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card text-center h-100">
+            <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <i class="bi bi-shield-lock display-4"></i>
+                <h6 class="mt-3">Security</h6>
+            </div>
+            <a href="/admin.security" class="stretched-link"></a>
+        </div>
+    </div>
+
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card text-center h-100">
+            <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <i class="bi bi-headset display-4"></i>
+                <h6 class="mt-3">Support</h6>
+            </div>
+            <a href="/admin.support" class="stretched-link"></a>
+        </div>
+    </div>
+
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card text-center h-100">
+            <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <i class="bi bi-folder-symlink display-4"></i>
+                <h6 class="mt-3">Data migration</h6>
+            </div>
+            <a href="/admin.data-migration" class="stretched-link"></a>
+        </div>
+    </div>
+</div>
