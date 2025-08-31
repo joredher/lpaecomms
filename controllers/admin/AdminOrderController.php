01dd671effb9c551fc453667935bf4a2f5c681e0
Eduardo Hernandez <joredher30@gmail.com>
2025-08-31 10:21:11 +1000

    fix: avoid admin order controller conflict

--- controllers/admin/AdminOrderController.php
+++ controllers/admin/AdminOrderController.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../../bootstrap.php';
 
 loadRepo('repositories/invoice/InvoiceRepository.php');
 
-class OrderController
+class AdminOrderController
 {
     public function updateStatus(): void
     {
--- includes/router.php
+++ includes/router.php
@@ -45,7 +45,7 @@ RegisterController::register('admin.products', ['index']);
 
 RegisterController::register($route, 'admin', 'UserController', ['create', 'store', 'edit', 'update', 'destroy']);
 RegisterController::register($route, 'admin', 'ProductController', ['types']);
-RegisterController::register($route, 'admin', 'OrderController', ['updateStatus', 'exportOrders']);
+RegisterController::register($route, 'admin', 'AdminOrderController', ['updateStatus', 'exportOrders']);
 
 // --- Helpers
 $ua    = $_SERVER['HTTP_USER_AGENT'] ?? '';
